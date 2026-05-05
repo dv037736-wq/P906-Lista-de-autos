d73e7de16a7635acffec9f7e5b1fe79839d4644c
Gustavo Orlando Galindo Villanueva <go458481@gmail.com>
2026-05-04 20:10:16 -0600

    centre, agregue titulo y funcion para mostrar menu

---
+++ funciones/imprimirMenu.php
@@ -0,0 +1,8 @@
+<?php
+function imprimirMenu() {
+    echo titulo("INVENTARIO DE AUTOS", 50);
+    echo "1 - Agregar auto \n";
+    echo "2 - Mostrar autos \n";
+    echo "3 - Eliminar auto \n";
+    echo "4 - Salir \n";
+}
